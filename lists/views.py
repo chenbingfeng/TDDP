ff285b6bed74bf18e983c14ce30580c64ef2f852
Bingfeng Chen <bingfeng.chen@salesforce.com>
2015-05-19 12:27:58 -0400

    new URL, view and template to display lists

--- lists/views.py
+++ lists/views.py
@@ -19,10 +19,11 @@ def home_page(request):
     if request.method == 'POST':
         #new_item_text = request.POST['item_text']
         Item.objects.create(text=request.POST['item_text'])
-        return redirect('/')
+        return redirect('/lists/the-only-list-in-the-world/')
+    return render(request, 'home.html')
 
-    items = Item.objects.all()
-    return render(request, 'home.html', {'items': items})
+    #items = Item.objects.all()
+    #return render(request, 'home.html', {'items': items})
 
     #else:
     #    new_item_text = ''
@@ -30,3 +31,7 @@ def home_page(request):
     #return render(request, 'home.html', {
     #    'new_item_text': new_item_text,
     #})
+
+def view_list(request):
+    items = Item.objects.all()
+    return render(request, 'list.html', {'items': items})
